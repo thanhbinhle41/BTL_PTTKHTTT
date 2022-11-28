c6b67df2da4f29b4e793c16b4711d9c0ca612642
Le Thanh Binh <thanhbinhle41@gmail.com>
2022-11-28 23:18:13 +0700

    first commit

---
+++ src/main/java/com/pttkhtt/pttk/BtlPttkApplication.java
@@ -0,0 +1,13 @@
+package com.pttkhtt.pttk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtlPttkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtlPttkApplication.class, args);
+	}
+
+}
